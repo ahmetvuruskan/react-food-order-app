12115eb3ee90abb910a44a43e836878e341ae810
ahmetvuruskan <ahmetvrskn@gmail.com>
2023-10-10 00:24:15 +0300

    Using Context and Adding Cart Reducex

--- src/component/Layout/HeaderCartButton.js
+++ src/component/Layout/HeaderCartButton.js
@@ -1,12 +1,18 @@
+import {useContext} from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./Styles/HeaderCartButton.module.css"
+import CardContext from "../../store/cart-context";
 
 
 const HeaderCartButton = (props) => {
+    const cartCtx = useContext(CardContext)
+    const numberOfCartItems = cartCtx.items.reduce((curNumber,item)=>{
+        return curNumber+item.amount
+    },0)
     return <button onClick={props.onClick} className={classes.button}>
         <span className={classes.icon}><CartIcon/></span>
         <span>Your Cart</span>
-        <span className={classes.badge}>$5.22</span>
+        <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
 }
 
--- src/store/CardProvider.js
+++ src/store/CardProvider.js
@@ -1,19 +1,48 @@
-import React from "react";
+import React, {useReducer} from "react";
 import CartContext from "./cart-context";
 
+const defaultCartState = {
+    items: [],
+    totalAmount: 0,
+}
+const cartReducer = (state, action) => {
+    if (action === 'ADD_ITEM') {
+        const updatedItems = state.items.concat(action.item)
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        }
+    }
+
+
+    return defaultCartState
+}
+
 const CardProvider = props => {
+
+    const [cartState, dispatcCartAction] = useReducer(cartReducer, defaultCartState,)
     const addItemToCartHandler = (item) => {
+        dispatcCartAction({
+            type: 'ADD_ITEM',
+            item: item
+        })
     }
     const removeItemFromCartHandler = (id) => {
+        dispatcCartAction({
+            type: "REMOVE_ITEM",
+            item:id
+
+        })
     }
 
     const cartContext = {
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: addItemToCartHandler,
         removeItem: removeItemFromCartHandler
     }
-    return <CartContext.Provider>
+    return <CartContext.Provider value={cartContext}>
         {props.children}
     </CartContext.Provider>
 }
